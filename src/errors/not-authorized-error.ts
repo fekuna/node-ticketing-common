a7effa616456e5cbeb26a7676f09112c8cb8e4fc
Alfan <alfan.almunawar@yummycorp.com>
2023-03-29 05:24:38 +0700

    first publish

---
+++ src/errors/not-authorized-error.ts
@@ -0,0 +1,13 @@
+import { CustomError } from './custom-error';
+
+export class NotAuthorizedError extends CustomError {
+  statusCode: number = 401;
+
+  constructor() {
+    super('Not Authorized');
+  }
+
+  serializeErrors(): { message: string; field?: string | undefined }[] {
+    return [{ message: 'Not authorized' }];
+  }
+}
---
+++ src/errors/not-found-error.ts
@@ -0,0 +1,12 @@
+import { CustomError } from './custom-error';
+
+export class NotFoundError extends CustomError {
+  statusCode = 404;
+  constructor() {
+    super('Route not found');
+  }
+
+  serializeErrors(): { message: string; field?: string | undefined }[] {
+    return [{ message: 'Not Found' }];
+  }
+}
